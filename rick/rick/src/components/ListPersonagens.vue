38e7b101a69f908dfa3ef9af9867f78183ccf060
GilmarGiroto <128000428+GilmarGiroto@users.noreply.github.com>
2024-04-29 19:30:28 -0300

    Add files via upload

---
+++ rick/rick/src/components/ListPersonagens.vue
@@ -0,0 +1,25 @@
+<script setup>
+const personagem = defineProps(["name", "url", "status", "specie", "gender", "location", "episode"])
+const personagemId = personagem.url.split('/')[5]
+const imageUrl = "https://rickandmortyapi.com/api/character/avatar/" + personagemId + ".jpeg"
+console.log(imageUrl)
+
+</script>
+
+<template>
+    <div class="col-md-3">
+        <div class="card p-2 mb-3">
+            <img :src="imageUrl" alt="">
+            <p class="text-center"> Nome: {{personagem.name}}</p>
+            <p class="text-center"> Status: {{personagem.status}}</p>
+            <p class="text-center"> Especie: {{personagem.specie}}</p>
+            <p class="text-center"> Gênero: {{personagem.gender}}</p>
+            <p class="text-center">Localização Atual: {{personagem.location}}</p>
+            <p class="text-center"> Episódio: {{personagem.episode}}</p>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ rick/rick/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <p>
+      Aluno: Gilmar Junior Giroto Ferreira<br>
+      RA: 1978685<br>
+      Curso: Análise e Desenvolvimento de Sistemas<br>
+      Turma: 3C<br>
+      Unimar
+    </p> 
+  </div>
+</template>
+
+<style>
+p {
+  margin-left: 10px
+}
+</style>
