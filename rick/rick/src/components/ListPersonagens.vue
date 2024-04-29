9b8d5c8009b6cad3a5a537217cbadf548bf20dd5
GilmarGiroto <128000428+GilmarGiroto@users.noreply.github.com>
2024-04-29 20:15:13 -0300

    API - Rick and Morty

---
+++ rick/rick/src/components/ListPersonagens.vue
@@ -0,0 +1,25 @@
+<script setup>
+const personagem = defineProps(["name", "url", "status", "specie", "gender", "location", "episode"])
+const personagemId = personagem.url.split('/')[5]
+const imageUrl = "https://rickandmortyapi.com/api/character/avatar/" + personagemId + ".jpeg"
+console.log(imageUrl)
+
+</script>
+
+<template>
+    <div class="col-md-3">
+        <div class="card p-2 mb-3">
+            <img :src="imageUrl" alt="">
+            <p class="text-center"><strong>Nome:</strong>  {{personagem.name}}</p>
+            <p class="text-center"><strong>Status:</strong>  {{personagem.status}}</p>
+            <p class="text-center"><strong>Especie:</strong>  {{personagem.specie}}</p>
+            <p class="text-center"><strong>Gênero:</strong>  {{personagem.gender}}</p>
+            <p class="text-center"><strong>Localização Atual:</strong> {{personagem.location}}</p>
+            <p class="text-center"> <strong>Aparições:</strong> {{personagem.episode.length}}</p>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
---
+++ rick/rick/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <div class="card" style="width: 25rem; margin-left: 40%;">
+      <img class="card-img-top" src="https://pngimg.com/d/rick_morty_PNG4.png" alt="Imagem de capa do card">
+      <div class="card-body">
+        <h5 class="card-title">Gilmar Junior Giroto Ferreira</h5><br>
+        <p>Análise e Desenvolvimento de Sistemas | Unimar</p><br>
+        <p>RA: 1978685</p><br>
+        <p>Turma: 3ºC</p><br>
+        <a href="https://github.com/GilmarGiroto/API-Rick-and-Morty" style="margin-left: 10px; text-decoration: none; color: black">Github</a>
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" style="width: 20px; margin-left: 5px;">
+      </div>
+        
+
+    </div>
+  </main>
+</template>
+
+<style>
+p {
+  margin-left: 10px
+}
+</style>
